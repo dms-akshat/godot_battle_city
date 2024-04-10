82bff318ac8602ea2df965f02c4c869163b0db6e
Akshat Chaturvedi <akshat.chaturvedi2003@gmail.com>
2024-04-10 16:35:25 +0530

    updated to Godot4

--- scripts/Tank.gd
+++ scripts/Tank.gd
@@ -1,10 +1,10 @@
-extends KinematicBody2D
+extends CharacterBody2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var velocity = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,5 +12,9 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	set_velocity(Vector2(0,0))
+	move_and_slide()
+	if(Input.is_action_pressed("ui_right")):
+		velocity.x = 20
+		print(velocity)
