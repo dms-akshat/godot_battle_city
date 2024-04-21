8bcda582ca1f7f211808ff94bb228d2f68da2bb1
MysteriousCoder7 <saksham.sks11@gmail.com>
2024-04-21 16:23:00 +0530

    FileDeleted

--- Level_1.tres.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-var spawn_scene=preload("res://scenes/Enemy.tscn")
-var positions=$SpawnLocations.get_children()
-func _on_tank_spawn_tank_spawn():
-	var enemy=spawn_scene.instantiate()
-	var selected_position=positions[randi()%positions.size()]
-	enemy.position=selected_position.global_position
-	$TankSpawn.add_child(enemy)
-	
