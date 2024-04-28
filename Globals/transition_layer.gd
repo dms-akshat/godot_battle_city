049f20d59d6f58b7e08d36490dd92d81dbb6577c
CodePokeX <sonta445@gmail.com>
2024-04-28 10:09:57 +0530

    Buttons issue solved

--- Globals/transition_layer.gd
+++ Globals/transition_layer.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 func change_scene(target: String) ->void:
+	#print($AnimationPlayer)
 	$AnimationPlayer.play("fade_to_black")
 	await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file(target)
